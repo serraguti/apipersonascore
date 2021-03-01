839905d1e9740429b5acc0febf3a2a47f23bff2a
Serra <pacoserranox@gmail.com>
2021-03-01 14:07:22 +0100

    Add project files.

---
+++ ApiPersonas/Controllers/PersonasController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ApiPersonas.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiPersonas.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonasController : ControllerBase
+    {
+        List<Persona> personas;
+        public PersonasController()
+        {
+            personas = new List<Persona>
+            {
+                new Persona { IdPersona = 1, Nombre = "Lucia", Edad = 17}
+                , new Persona { IdPersona = 2, Nombre = "Adrian", Edad = 20}
+                , new Persona { IdPersona = 3, Nombre = "Sara", Edad = 24}
+                , new Persona {IdPersona = 4, Nombre = "Pedro", Edad = 40}
+                , new Persona { IdPersona = 5, Nombre = "Carlos", Edad = 40 }
+            };
+        }
+
+        [HttpGet]
+        public ActionResult<List<Persona>> Get()
+        {
+            return this.personas;
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Persona> GetPersona(int id)
+        {
+            return this.personas.SingleOrDefault(x => x.IdPersona == id);
+        }
+    }
+}
+
---
+++ ApiPersonas/Startup.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+
+namespace ApiPersonas
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSwaggerGen(
+                options =>
+                {
+                    options.SwaggerDoc(
+                        name: "v1", new OpenApiInfo
+                        {
+                             Title = "Primer Api Personas 2021", 
+                             Version = "v1", 
+                             Description = "Hoy es un maravillo lunes lluvioso"
+                        });
+                });
+            services.AddControllers();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseSwagger();
+            app.UseSwaggerUI(
+                options =>
+                {
+                    options.SwaggerEndpoint(
+                        url: "/swagger/v1/swagger.json", name: "Api v1");
+                    options.RoutePrefix = "";
+                });
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
